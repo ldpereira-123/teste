c60153828696fdd476b82393a549e6ac277ad02c
Leandro de Matos Pereira <ldpereira@uolinc.com>
2019-05-02 12:32:47 -0300

    criado o servidor api com express

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+	res.status(200).send({ message: 'olá mundo estou funcionando!' });
+})
+
+
+app.listen(3001, () => {
+	console.log('Api rodando na porta 3001');
+})
+	
