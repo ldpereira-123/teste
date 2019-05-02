b942bee723fbefe1fb33738a5d653cac0fd0feb4
ldpereira-123 <45997463+ldpereira-123@users.noreply.github.com>
2019-05-02 12:52:33 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ app.get('/', (req, res) => {
 	res.status(200).send({ message: 'olá mundo estou funcionando!' });
 })
 
+app.get('/ola', (req, res) => {
+	res.status(200).send({ message: 'Eu sou o Olá' });
+})
 
 app.listen(3001, () => {
 	console.log('Api rodando na porta 3001');
